0f4cb7711ea194d59435fd18c6a6f65b28b064b3
unknown <ibmjavanh09@iiht.tech>
2021-04-26 17:06:59 +0530

    integrated with REST Api

--- src/userform/userform.js
+++ src/userform/userform.js
@@ -1,23 +1,35 @@
 import { useState } from "react";
+import axios from "axios";
 
 
 function Userform(){ //component, controller
     //model
-    const[userform, setUserform]=useState({
-        name:'Ram'
-    })
+    const[userform, setUserform]=useState({  // usestate=hook,pre-defined function
+        name:"Ram",
+        phoneNumber:"1"
+    });
+    const save= function(event){
+        console.log(userform.name);
+       axios.post('http://localhost:8080/user', userform)
+       .then(function(response) {
+           console.log(response);
+       })
+       .catch(function(error){
+           console.log(error);
+       })       
+    }
     return (
         <div>
-            <input value={userform.name} onChange={handleEvent}></input>
+            <input value={userform.name} name="name" onChange={updateState}></input>
+            <input value={userform.phoneNumber} name="phoneNumber" placeholder="phoneNumber" onChange={updateState}></input>
+            <button onClick={save}>Save</button>
         </div>
     )
-   function handleEvent(event){
-       console.log(event);
-       setUserform({
-           name:event.target.value
-       })
-      // name=event.target.value;
-      //setState
+   function updateState(event){
+       const current={...userform
+       ,[event.target.name]:event.target.value};
+       console.log(current);
+       setUserform(current);
     }
 }
 
